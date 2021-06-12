e67a94e98936fd0adcfed3b5f4fdf5d2b374fcac
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-06-12 23:33:30 +0500

    Lecture 10: Drawer

--- Lectures/Class 10 Advanced Widgets/advanced_widgets/lib/pages/home.dart
+++ Lectures/Class 10 Advanced Widgets/advanced_widgets/lib/pages/home.dart
@@ -37,21 +37,64 @@ class HomeScreen extends StatelessWidget {
       // ),
 
       // GridView
-      body: GridView.count(
-          crossAxisCount: 3,
-          crossAxisSpacing: 50,
-          mainAxisSpacing: 50,
-          children: List.generate(nameList.length, (index) {
-            return Container(
-              height: 200,
-              width: 200,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10)),
-              child: Text(nameList[index]),
-            );
-          })),
+
+      // body: GridView.count(
+      //     crossAxisCount: 3,
+      //     crossAxisSpacing: 50,
+      //     mainAxisSpacing: 50,
+      //     children: List.generate(nameList.length, (index) {
+      //       return Container(
+      //         height: 200,
+      //         width: 200,
+      //         alignment: Alignment.center,
+      //         decoration: BoxDecoration(
+      //             color: Colors.grey.withOpacity(0.2),
+      //             borderRadius: BorderRadius.circular(10)),
+      //         child: Text(nameList[index]),
+      //       );
+      //     })),
+
+      // DRAWER EXAMPLE
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                curve: Curves.easeInCirc,
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  color: Colors.orange,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'),
+                      ),
+                      SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Muhammad Huzaifa',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text('Huzaifanadir@gmail.com')
+                        ],
+                      )
+                    ],
+                  ),
+                )),
+            ListTile(title: Text('Home')),
+            ListTile(title: Text('Gallery')),
+            ListTile(title: Text('About')),
+            ListTile(title: Text('Contact Us')),
+            Divider(),
+            ListTile(title: Text('Logout')),
+          ],
+        ),
+      ),
     );
   }
 

48e2f79276fe9c5f06af6f9c7e6ff0047f6bab54
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-06-20 21:25:03 +0500

    ListView & GridView Together Problem Solution.

--- Lectures/Class 11 Calculator And TodoApp/class_eleven/lib/home.dart
+++ Lectures/Class 11 Calculator And TodoApp/class_eleven/lib/home.dart
@@ -1,10 +1,94 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  List<dynamic> list = [1, 2, 3, 4, 5, 6, 7, 8];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('HomeScreen')),
+      body: SingleChildScrollView(
+        // child: Column(
+        //   children: [
+        //     Container(
+        //       color: Colors.red,
+        //       height: 200,
+        //       width: 200,
+        //       margin: EdgeInsets.only(top: 10),
+        //     ),
+        //     Container(
+        //       color: Colors.green,
+        //       height: 200,
+        //       width: 200,
+        //       margin: EdgeInsets.only(top: 10),
+        //     ),
+        //     Container(
+        //       color: Colors.blue,
+        //       height: 200,
+        //       width: 200,
+        //       margin: EdgeInsets.only(top: 10),
+        //     ),
+        //     Container(
+        //       color: Colors.yellow,
+        //       height: 200,
+        //       width: 200,
+        //       margin: EdgeInsets.only(top: 10),
+        //     ),
+
+        //    ],
+        // ),
+
+        child: Column(
+          children: [
+            Container(
+              height: 210,
+              child: ListView(
+                // physics: NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    height: 200,
+                    width: 200,
+                    margin: EdgeInsets.only(top: 10),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 200,
+                    width: 200,
+                    margin: EdgeInsets.only(top: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 200,
+                    width: 200,
+                    margin: EdgeInsets.only(top: 10),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    height: 200,
+                    width: 200,
+                    margin: EdgeInsets.only(top: 10),
+                  ),
+                ],
+              ),
+            ),
+            GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                physics: NeverScrollableScrollPhysics(),
+                children: List.generate(list.length, (index) {
+                  return Container(
+                    color: Colors.red,
+                    child: Text(list[index].toString()),
+                  );
+                }))
+          ],
+        ),
+      ),
     );
   }
 }
